686cdabf44cc04346bb6de9dd2ee7872dca0b0ea
Adam Shepherd <ashepherd@whoi.edu>
2020-07-28 08:15:02 -0400

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,11 @@
-var sjs = SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: '/search.json'
-})
+$(document).ready( function(){
+  var sjs = SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '/search.json'
+  });
+  
+  $('#search-input').change(function() {
+    sjs.search( $( this ).val() );
+  });
+});
